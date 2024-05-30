9c375c8d8ce181d3e2dce4420569cd96fd9eba99
idatenkiddy <idatengamerz@gmail.com>
2024-05-30 17:38:13 +0530

    a

--- Projects/Project4/script.js
+++ Projects/Project4/script.js
@@ -1,85 +1,3 @@
-// const imgarr = [
-//   {
-//     name: "Toilet",
-//     image:
-//       "https://images.unsplash.com/photo-1589824783837-6169889fa20f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   },
-//   {
-//     name: "Dodge Car",
-//     image:
-//       "https://images.unsplash.com/photo-1609386464913-4cbfa39de540?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   },
-//   {
-//     name: "Ford Car",
-//     image:
-//       "https://images.unsplash.com/photo-1551501419-cb31cdd588d6?q=80&w=929&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   },
-//   {
-//     name: "Cat",
-//     image:
-//       "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=843&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   },
-//   {
-//     name: "Dog",
-//     image:
-//       "https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   },
-//   {
-//     name: "Porsche Taycan",
-//     image:
-//       "https://imgd.aeplcdn.com/1280x720/n/cw/ec/45063/taycan-exterior-right-front-three-quarter-5.jpeg?isig=0&q=80",
-//   },
-// ];
-
-// function showTheCards() {
-//   var htmladder = "";
-//   imgarr.forEach(function (obj) {
-//     htmladder += `<div class="box">
-//               <img class="cursor-pointer" src="${obj.image}" alt="">
-//               <div class="caption"></div>`;
-//   });
-
-//   document.querySelector(".container").innerHTML = htmladder;
-// }
-
-// function handleSearchFuntionality() {
-//   let input = document.querySelector("#searchinput");
-//   let overlay = document.querySelector(".overlay");
-
-//   input.addEventListener("focus", function () {
-//     overlay.style.display = "block";
-//   });
-
-//   input.addEventListener("blur", function () {
-//     overlay.style.display = "none";
-//     document.querySelector(".searchdata").style.display = "none";
-//   });
-
-//   input.addEventListener("input", function () {
-//     searchValue = input.value.toLowerCase();
-//     // if (!searchValue == 0) console.log(searchValue);
-
-//     const filterd_imgarr = imgarr.filter((obj) =>
-//       obj.name.toLowerCase().startsWith(searchValue)
-//     );
-
-//     var clutter = "";
-
-//     filterd_imgarr.forEach(function (obj) {
-//       clutter += `<div class="res flex px-8 py-3">
-//       <i class="ri-search-line font-semibold mr-5"></i>
-//       <h3 class="font-semibold">${obj.name.toLowerCase()}</h3>
-//       </div>`;
-//     });
-//     document.querySelector(".searchdata").style.display = "block";
-//     document.querySelector(".searchdata").innerHTML = clutter;
-//   });
-// }
-
-// handleSearchFuntionality();
-// showTheCards();
-
-
 const imgarr = [
   {
     name: "Toilet",
@@ -110,6 +28,93 @@ const imgarr = [
     name: "Porsche Taycan",
     image:
       "https://imgd.aeplcdn.com/1280x720/n/cw/ec/45063/taycan-exterior-right-front-three-quarter-5.jpeg?isig=0&q=80",
+  },
+  {
+    name: "Roblox Boxing",
+    image:
+      "https://tr.rbxcdn.com/5a4b28d22087fc8fe1342ac087e8ee3e/768/432/Image/Webp",
+  },
+  {
+    name: "Web Dev",
+    image: "https://img.youtube.com/vi/_rmSp20X4wo/maxresdefault.jpg",
+  },
+  {
+    name: "Toilet",
+    image:
+      "https://images.unsplash.com/photo-1589824783837-6169889fa20f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Dodge Car",
+    image:
+      "https://images.unsplash.com/photo-1609386464913-4cbfa39de540?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Ford Car",
+    image:
+      "https://images.unsplash.com/photo-1551501419-cb31cdd588d6?q=80&w=929&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Cat",
+    image:
+      "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=843&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Dog",
+    image:
+      "https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Porsche Taycan",
+    image:
+      "https://imgd.aeplcdn.com/1280x720/n/cw/ec/45063/taycan-exterior-right-front-three-quarter-5.jpeg?isig=0&q=80",
+  },
+  {
+    name: "Roblox Boxing",
+    image:
+      "https://tr.rbxcdn.com/5a4b28d22087fc8fe1342ac087e8ee3e/768/432/Image/Webp",
+  },
+  {
+    name: "Web Dev",
+    image: "https://img.youtube.com/vi/_rmSp20X4wo/maxresdefault.jpg",
+  },
+  {
+    name: "Toilet",
+    image:
+      "https://images.unsplash.com/photo-1589824783837-6169889fa20f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Dodge Car",
+    image:
+      "https://images.unsplash.com/photo-1609386464913-4cbfa39de540?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Ford Car",
+    image:
+      "https://images.unsplash.com/photo-1551501419-cb31cdd588d6?q=80&w=929&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Cat",
+    image:
+      "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=843&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Dog",
+    image:
+      "https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Porsche Taycan",
+    image:
+      "https://imgd.aeplcdn.com/1280x720/n/cw/ec/45063/taycan-exterior-right-front-three-quarter-5.jpeg?isig=0&q=80",
+  },
+  {
+    name: "Roblox Boxing",
+    image:
+      "https://tr.rbxcdn.com/5a4b28d22087fc8fe1342ac087e8ee3e/768/432/Image/Webp",
+  },
+  {
+    name: "Web Dev",
+    image: "https://img.youtube.com/vi/_rmSp20X4wo/maxresdefault.jpg",
   },
 ];
 
@@ -154,6 +159,13 @@ function handleSearchFunctionality() {
       </div>`;
     });
 
+    if (filtered_imgarr.length === 0) {
+      clutter = `<div class="res flex px-8 py-3">
+      <i class="ri-search-line font-semibold mr-5"></i>
+      <h3 class="font-semibold">Not Found 404</h3>
+      </div>`;
+    }
+
     document.querySelector(".searchdata").style.display = "block";
     document.querySelector(".searchdata").innerHTML = clutter;
 
@@ -161,5 +173,18 @@ function handleSearchFunctionality() {
   });
 }
 
-handleSearchFunctionality();
-showTheCards(imgarr);
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function initialize() {
+  // Randomize the array
+  shuffleArray(imgarr);
+  showTheCards(imgarr);
+  handleSearchFunctionality();
+}
+
+initialize();
